d67ddf92b85d8540cbda803c7a2f98955c4f35aa
William Parry <williamparry@gmail.com>
2020-06-21 15:53:32 +1000

    de-abstract pdf control as it's only used in one place

--- public/human-refinement/index.js
+++ public/human-refinement/index.js
@@ -1,4 +1,3 @@
-import pdfControl from '../global/pdfControl.js';
 import { $ } from '../global/utilities.js';
 
 const MINIMAP_SCALE = 0.2;
@@ -62,6 +61,61 @@ const months = [
 	'November',
 	'December',
 ];
+
+const renderPDFCanvas = (attachToEl, viewport, page) => {
+	let pdfCanvas = document.createElement('canvas');
+	attachToEl.appendChild(pdfCanvas);
+	let pdfContext = pdfCanvas.getContext('2d');
+	pdfCanvas.style.display = 'block';
+	pdfCanvas.height = viewport.height;
+	pdfCanvas.width = viewport.width;
+
+	page.render({
+		canvasContext: pdfContext,
+		viewport: viewport,
+	});
+};
+
+const loadPage = async (pageNumber, state) => {
+	const page = await state.doc.getPage(pageNumber);
+
+	var pdfViewport = page.getViewport({ scale: 1.0 });
+	var pdfMinimapViewport = page.getViewport({ scale: state.scale });
+
+	renderPDFCanvas(state.pdfEl, pdfViewport, page);
+	renderPDFCanvas(state.pdfMinimapEl, pdfMinimapViewport, page);
+
+	if (state.pageWidth == -1) {
+		state.pageWidth = pdfViewport.width;
+		state.pageHeight = pdfViewport.height;
+		state.docHeight = state.pageHeight * state.numPages;
+	}
+
+	return state;
+};
+
+const pdfControl = async (pdfURL, pdfEl, pdfMinimapEl, scale) => {
+	const loadingTask = pdfjsLib.getDocument(pdfURL);
+	const doc = await loadingTask.promise;
+	const numPages = doc.numPages;
+
+	const state = {
+		doc,
+		pdfEl,
+		pdfMinimapEl,
+		pageWidth: -1,
+		pageHeight: -1,
+		docHeight: -1,
+		numPages: doc.numPages,
+		scale,
+	};
+
+	for (let i = 0; i < numPages; i++) {
+		await loadPage(i + 1, state);
+	}
+
+	return state;
+};
 
 const search = async (searchQuery, caseData) => {
 	return new Promise((resolve) => {
@@ -452,7 +506,7 @@ const loadFacet = (facetId) => {
 				span.appendChild(button);
 				el.appendChild(span);
 				el.classList.add('hoverable');
-				if(dates.indexOf(pointWrap.wrap.dataset.option) == -1) {
+				if (dates.indexOf(pointWrap.wrap.dataset.option) == -1) {
 					dates.push(pointWrap.wrap.dataset.option);
 				}
 			}
