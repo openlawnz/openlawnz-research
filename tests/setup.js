23541476aeb8199499dd34c25f5511f288e7bcf0
Daniel Jimenez <daniel.jimenez@spokephone.com>
2020-07-25 16:14:37 +1200

    Remove PDFController tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,2 +1,2 @@
-global.pdfjsLib = require("pdfjs-dist/es5/build/pdf.js");
-global.canvas = require("canvas");
+global.pdfjsLib = require('pdfjs-dist/es5/build/pdf.js');
+global.canvas = require('canvas');
